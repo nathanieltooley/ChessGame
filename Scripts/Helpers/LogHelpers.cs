51e86e5a4151eaa1f64bee058f0980dc6fe96cbc
Nathaniel Tooley <gotablast99@gmail.com>
2023-07-20 18:16:34 -0500

    added log helpers file

---
+++ Scripts/Helpers/LogHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessGame.Scripts.Helpers
+{
+    public static class LogHelpers
+    {
+        public static void DebugLog(string message)
+        {
+            Debugger.Log(2, "inf", message);
+        }
+    }
+}
