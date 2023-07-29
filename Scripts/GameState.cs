8b9a25969b0f4e858f6f99cfaec8e1e074e1f1b5
Nathaniel Tooley <gotablast99@gmail.com>
2023-07-29 16:31:08 -0500

    added gamestate script

--- Scripts/GameState.cs
+++ Scripts/GameState.cs
@@ -1,15 +1,13 @@
+using ChessGame.Scripts;
 using Godot;
 using System;
 
 public partial class GameState : Node
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+    public ChessState CurrentGameState { get; set; }
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
 	{
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+        CurrentGameState = ChessState.NormalGame;
 	}
 }
