73d29e9c900fb6f0637e786ea15cfd796a10a26c
Nathaniel Tooley <gotablast99@gmail.com>
2023-08-09 14:52:19 -0500

    created a controller factory

---
+++ Scripts/Factories/ControllerFactory.cs
@@ -0,0 +1,21 @@
+﻿
+using ChessGame.Scripts.Controllers;
+using Godot;
+
+namespace ChessGame.Scripts.Factories
+{
+    public static class ControllerFactory
+    {
+        public static Node RootNode { get; set; }
+
+        public static BoardController GetBoardController()
+        {
+            return RootNode.GetNode<BoardController>("/root/Main/Controllers/BoardController");
+        }
+
+        public static PlayerMovementController GetPlayerMovementController()
+        {
+            return RootNode.GetNode<PlayerMovementController>("/root/Main/Controllers/PlayerInputController");
+        }
+    }
+}
