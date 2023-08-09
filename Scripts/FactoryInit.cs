423d388f80a4fd1825306edad0e5ce777f5aec03
Nathaniel Tooley <gotablast99@gmail.com>
2023-08-09 14:52:36 -0500

    added a factoryinit node

---
+++ Scripts/FactoryInit.cs
@@ -0,0 +1,15 @@
+using ChessGame.Scripts.Factories;
+using Godot;
+
+public partial class FactoryInit : Node
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var rootNode = GetNode<Node>("/root/Main");
+
+		ServiceFactory.RootNode = rootNode;
+		ControllerFactory.RootNode = rootNode;
+    }
+
+}
