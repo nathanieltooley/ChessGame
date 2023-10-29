f4a528d395180a9cc9ac8abaa187d427693ed7ec
Nathaniel Tooley <gotablast99@gmail.com>
2023-10-29 15:24:21 -0500

    Factored out code from endgame handler

--- Scripts/Controllers/EndgameHandler.cs
+++ Scripts/Controllers/EndgameHandler.cs
@@ -19,6 +19,8 @@ namespace ChessGame.Scripts.Controllers
         {
             _board = board;
             _moveCache = moveCache;
+
+            BlockingPieces = new List<BoardPos>();
         }
 
         public bool CheckCheck(BoardPos kingPos, ChessColor attackerColor)
@@ -28,29 +30,10 @@ namespace ChessGame.Scripts.Controllers
 
         public bool CheckMateCheck(BoardPos kingPos, ChessColor attackerColor, List<BoardPos> kingsMoves)
         {
-            List<BoardPos> attackerPositions = new List<BoardPos>();
             // Check to see if king's position is under attack
             if (!MoveHelpers.IsTileUnderAttack(_board, kingPos, attackerColor, _moveCache))
             {
                 return false;
-            }
-
-            for (int rank = 0; rank < 8; rank++)
-            {
-                for (int file = 0; file < 8; file++)
-                {
-                    PieceInfo tileInfo = BoardDataHandler.GetPieceInfoAtPos(_board, new BoardPos(rank, file));
-
-                    if (tileInfo.PieceId == ChessPieceId.Empty)
-                    {
-                        continue;
-                    }
-
-                    if (_moveCache[rank, file].FindAll(x => x.Rank == kingPos.Rank && x.File == kingPos.File).Count > 0)
-                    {
-                        attackerPositions.Add(new BoardPos(rank, file));
-                    }
-                }
             }
 
             // Check all of the king's moves to see if those are under attack as well
@@ -63,31 +46,20 @@ namespace ChessGame.Scripts.Controllers
             }
 
             // Check if any of the tiles between the attacker and the king are able to be blocked by friendly pieces
+            bool canBeBlocked = false;
 
-            foreach (var attackerPos in attackerPositions)
+            var attackers = BoardSearching.GetAllAttackerPositions(_board, kingPos, _moveCache);
+
+            foreach (var attacker in attackers)
             {
-                PieceInfo attacker = BoardDataHandler.GetPieceInfoAtPos(_board, attackerPos);
-
-                // Skip knight since he can jump over pieces
-                if (attacker.PieceId == ChessPieceId.Knight)
+                if (BoardSearching.GetAllBlockingPiecePositions(_board, _moveCache, kingPos, attacker).Count != 0)
                 {
-                    continue;
-                }
-
-                Vector2I line = MoveHelpers.GetLineMoveIsOn(attackerPos.Rank, attackerPos.File, kingPos.Rank, kingPos.File);
-
-                foreach (BoardPos pos in MoveHelpers.GetSpacesOnLine(attackerPos, line, _board))
-                {
-                    if (MoveHelpers.IsTileUnderAttack(_board, pos, MiscHelpers.InvertColor(attackerColor), _moveCache))
-                    {
-                        return false;
-                    }
+                    canBeBlocked = true;
                 }
             }
+            
 
-
-
-            return true;
+            return !canBeBlocked;
         }
     }
 }
