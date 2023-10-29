bcafa7a9c533b90747c6c39b01e1e0b7cdfa5e5a
Nathaniel Tooley <gotablast99@gmail.com>
2023-10-28 21:22:05 -0500

    Moved tile under attack function into helpers

--- Scripts/Helpers/MoveHelpers.cs
+++ Scripts/Helpers/MoveHelpers.cs
@@ -1,6 +1,7 @@
 ﻿using ChessGame.Scripts.Boards;
 using ChessGame.Scripts.ChessBoard;
 using ChessGame.Scripts.DataTypes;
+using ChessGame.Scripts.Factories;
 using Godot;
 using System;
 using System.Collections.Generic;
@@ -245,6 +246,57 @@ namespace ChessGame.Scripts.Helpers
             }
 
             return false;
+        }
+
+        public static bool IsTileUnderAttack(PieceInfo[,] board, BoardPos boardPos, ChessColor attackerColor, List<BoardPos>[,] moveCache)
+        {
+            for (int rank = 0; rank < 8; rank++)
+            {
+                for (int file = 0; file < 8; file++)
+                {
+                    var pos = new BoardPos(rank, file);
+
+                    PieceInfo piece = BoardDataHandler.GetPieceInfoAtPos(board, pos);
+                    if (piece.PieceId == ChessPieceId.Empty || piece.Color != attackerColor)
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        var moves = moveCache[pos.Rank, pos.File];
+                        foreach (var move in moves)
+                        {
+                            if (move == boardPos)
+                            {
+                                return true;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        public static List<BoardPos>[,] CreateMoveCache(PieceInfo[,] board)
+        {
+            List<BoardPos>[,] boardPosMatrix = new List<BoardPos>[8, 8];
+
+            for (int rank = 0; rank < 8; rank++)
+            {
+                for (int file = 0; file < 8; file++)
+                {
+                    BoardPos piecePos = new BoardPos(rank, file);
+                    PieceInfo pieceInfo = BoardDataHandler.GetPieceInfoAtPos(board, piecePos);
+
+                    if (pieceInfo.PieceId != ChessPieceId.Empty)
+                    {
+                        boardPosMatrix[rank, file] = MoveFinder.GetCapableMoves(piecePos, pieceInfo, board, ServiceFactory.GetGameInfoService());
+                    }
+                }
+            }
+
+            return boardPosMatrix;
         }
     }
 }
